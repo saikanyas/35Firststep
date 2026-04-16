5ee9c9099813c1bcc927b35496981ed7f612eb20
saikanyas <121143956+saikanyas@users.noreply.github.com>
2026-04-16 12:46:03 +0700

    v1.0.0 - Initial release
    v1.0.0 - Initial release

---
+++ 35Firststep.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("saikanyas.35firstStep.ciTiaoPatch", "35 First step", "1.0.0")]
+public class InitGameCiTiaoPatch : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        var harmony = new Harmony("saikanyas.35firstStep.ciTiaoPatch");
+        harmony.PatchAll();
+        Logger.LogInfo("35 First step loaded.");
+    }
+}
+
+[HarmonyPatch(typeof(InitGameUI), "InitData")]
+public static class Patch_InitGameUI_InitData
+{
+    private static readonly AccessTools.FieldRef<InitGameUI, int> CiTiaoPointRef =
+        AccessTools.FieldRefAccess<InitGameUI, int>("CiTaio_Point_Have");
+
+    [HarmonyPostfix]
+    public static void Postfix(InitGameUI __instance)
+    {
+        CiTiaoPointRef(__instance) = 35;
+        Debug.Log("[Patch] CiTaio_Point_Have set to 35");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# 36Firststep
+# 35Firststep
 A mod that adds 35 starting points by default during character creation.
+
+## Requirements
+- Game version: 0.9.0
+- BepInEx
+
+## Installation
+1. Install BepInEx
+2. Overwrite the BepInEx folder in the game's root directory with the one from this package
+3. Launch the game
+
